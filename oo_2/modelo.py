93a15a8de83b972abf68912fd7adffd3fdfa72f0
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-04-17 11:16:53 -0300

    oo_2 aula3

--- oo_2/modelo.py
+++ oo_2/modelo.py
@@ -19,11 +19,17 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes}'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes}'
 
     
 
@@ -32,15 +38,19 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes}'
+
     
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
 
-
-atlanta = Serie('atlanta', 2018, 2)
+atlanta = Serie('atlanta', 2019, 2)
 atlanta.dar_like()
 atlanta.dar_like()
 
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+for programa in filmes_e_series:
+    print(programa)
